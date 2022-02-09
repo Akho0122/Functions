0f3865445d68a20b924d46cde2e1adf9ce3a87f1
Akho0122 <mr.akhror.dev@gmail.com>
2022-02-09 21:32:02 +0500

    Function default parametres

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum = 1,
+  numPassengers = 1,
+  price = 299 * numPassengers
+) {
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+createBooking('LH123', undefined, 1000);
