a9d20b98c4bd718671c4ccf5c298725c407b6248
Akho0122 <mr.akhror.dev@gmail.com>
2022-02-12 20:22:21 +0500

    Bind part 1

--- script.js
+++ script.js
@@ -146,3 +146,23 @@ console.log(swiss);
 const flightData = [583, 'George Cooper'];
 book.apply(swiss, flightData);
 console.log(swiss);
+book.call(swiss, ...flightData);
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+bookEW(123, 'Steven Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Jonas');
+bookEW23('Martha');
+
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
