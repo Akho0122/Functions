c0b2d3bbdc79e842146a82eace974cdaaaa1053c
Akho0122 <mr.akhror.dev@gmail.com>
2022-02-15 17:03:42 +0500

    Callback function

--- script.js
+++ script.js
@@ -1,39 +1,62 @@
 'use strict';
 
-const bookings = [];
+// const bookings = [];
 
-const createBooking = function (flightNum = 0, numPassengers = 1, price = 1) {
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
-};
-createBooking('LH123');
-createBooking('LH123', 3, 800);
+// const createBooking = function (flightNum = 0, numPassengers = 1, price = 1) {
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+// createBooking('LH123');
+// createBooking('LH123', 3, 800);
 
-const flight = 'LH123';
-const jonas = {
-  name: 'John Smith',
-  password: 1234567654,
-};
-const checkIn = function (flightNum, passenger) {
-  flightNum = 'LH999';
-  passenger.name = 'Mr. ' + passenger.name;
-  if (passenger.password === 1234567654) {
-    alert('Checked In');
-  } else {
-    alert('Wrong Passport');
-  }
-};
-checkIn(flight, jonas);
-console.log(flight);
-console.log(jonas);
+// const flight = 'LH123';
+// const jonas = {
+//   name: 'John Smith',
+//   password: 1234567654,
+// };
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
+//   if (passenger.password === 1234567654) {
+//     alert('Checked In');
+//   } else {
+//     alert('Wrong Passport');
+//   }
+// };
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
 
-const newPassport = function (person) {
-  person.password = Math.trunc(Math.random() * 1000000000);
+// const newPassport = function (person) {
+//   person.password = Math.trunc(Math.random() * 1000000000);
+// };
+// newPassport(jonas);
+// checkIn(flight, jonas);
+
+const oneWord = function (str) {
+  return str.replaceAll(' ', '').toLowerCase();
 };
-newPassport(jonas);
-checkIn(flight, jonas);
+const upperFirstWord = function (str) {
+  const [first, ...other] = str.split(' ');
+  return [first.toUpperCase(), ...other].join(' ');
+};
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by ${fn.name}`);
+};
+
+transformer('JavaScript is the best', upperFirstWord);
+transformer('JavaScript is the best', oneWord);
+
+const high5 = function () {
+  console.log('HI');
+};
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
